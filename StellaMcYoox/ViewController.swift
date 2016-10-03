5ff0456d3647780c24e6619f159c70b8d7d18c0a
CPM Development <cpmvideo@mac-mini-di-cpm-2.local>
2016-10-03 11:39:02 +0200

    Initial Commit

---
+++ StellaMcYoox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StellaMcYoox
+//
+//  Created by CPM Development on 03/10/16.
+//  Copyright © 2016 CPM Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
